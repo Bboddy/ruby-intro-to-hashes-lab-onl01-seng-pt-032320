1deb6755f9da7b53adee49b5873524f04aed4b0c
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 12:23:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,6 @@ def monopoly_with_third_tier
         }
       }
     }
-  }
 end
 
 def monopoly_with_fourth_tier
@@ -74,5 +73,4 @@ def monopoly_with_fourth_tier
         }
       }
     }
-  }
 end

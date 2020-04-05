6254bad9fd696b850357adb85cfd348aa320754e
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 12:21:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,13 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	#copy and paste the monopoly hash you monopoly = {
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {}
+    }
+  }
 end
 
 def monopoly_with_third_tier
